7417523b8860ae98c87fbac16e751e2003ebf177
¨rinawasabi¨ <ru0909anmi@gmail.com>
2025-01-20 20:03:28 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Smooth scroll functionality
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Update year in footer dynamically
+document.querySelector('footer p').innerHTML += ` - ${new Date().getFullYear()}`;
